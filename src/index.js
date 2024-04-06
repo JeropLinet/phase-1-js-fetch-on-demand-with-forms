3b39b248951e970f59fc3230c60265557e364ab5
Linet <teweilinet@gmail.com>
2024-04-06 14:45:15 +0300

    fetch forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const init = () => {
   
-}
+        function show(value){
+            console.log(value);
+        }
+      let searchForm = document.querySelector('form');
+    
+      searchForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+    
+        const input = document.querySelector("input#searchByID");
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data.title);
+          const title = document.querySelector("section#movieDetails h4");
+          const summary = document.querySelector("section#movieDetails p");
+          
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+        });
+      });
+    
+    }
+    
+   
+
 
 document.addEventListener('DOMContentLoaded', init);
